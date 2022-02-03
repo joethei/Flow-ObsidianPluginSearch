6e9570450bd952d467e5c6a58171cc3f4699d478
Johannes Theiner <kontakt@joethei.xyz>
2022-02-03 11:43:08 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-This is an example Python plugin project which demonstrates the interaction of request from Flow and then call back to the plugin.
+## Tag & Word Cloud
 
-You can install this plugin via `pm install Hello World Python`
+Plugin for [Flow Launcher](https://www.flowlauncher.com/)
+
+
+![GitHub](https://img.shields.io/github/license/joethei/Flow-ObsidianPluginSearch)
+[![libera manifesto](https://img.shields.io/badge/libera-manifesto-lightgrey.svg)](https://liberamanifesto.com)
+---
+
+Search all Obsidian plugins and copy text to the clipboard in the format
+`_Plugin Name_(ObisdianURI|GH_Repo)`
+when selecting.
+
+![Demo](https://i.joethei.space/Flow.Launcher_lkLIIZmrGg.png)
+
+Trigger is `obs`
+
+
+I personally use this when helping in the Obsidian Community Discord to quickly get links.
+
+
+---
+
+This plugin will not be submitted to the flow plugin store as the use case is very
+specific.
--- main.py
+++ main.py
@@ -1,45 +1,101 @@
 # -*- coding: utf-8 -*-
 
-import sys,os
+import sys, os
+
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
-from flowlauncher import FlowLauncher
-import webbrowser
+from flox import Flox
+import urllib.request
+import json
+
+####
+# Copy to clipboard function (put) compatible with x64 from https://forums.autodesk.com/t5/maya-programming/ctypes-bug-cannot-copy-data-to-clipboard-via-python/td-p/9195866
+import ctypes
+from ctypes import wintypes
+
+CF_UNICODETEXT = 13
+
+user32 = ctypes.WinDLL('user32')
+kernel32 = ctypes.WinDLL('kernel32')
+
+OpenClipboard = user32.OpenClipboard
+OpenClipboard.argtypes = wintypes.HWND,
+OpenClipboard.restype = wintypes.BOOL
+CloseClipboard = user32.CloseClipboard
+CloseClipboard.restype = wintypes.BOOL
+EmptyClipboard = user32.EmptyClipboard
+EmptyClipboard.restype = wintypes.BOOL
+GetClipboardData = user32.GetClipboardData
+GetClipboardData.argtypes = wintypes.UINT,
+GetClipboardData.restype = wintypes.HANDLE
+SetClipboardData = user32.SetClipboardData
+SetClipboardData.argtypes = (wintypes.UINT, wintypes.HANDLE)
+SetClipboardData.restype = wintypes.HANDLE
+
+GlobalLock = kernel32.GlobalLock
+GlobalLock.argtypes = wintypes.HGLOBAL,
+GlobalLock.restype = wintypes.LPVOID
+GlobalUnlock = kernel32.GlobalUnlock
+GlobalUnlock.argtypes = wintypes.HGLOBAL,
+GlobalUnlock.restype = wintypes.BOOL
+GlobalAlloc = kernel32.GlobalAlloc
+GlobalAlloc.argtypes = (wintypes.UINT, ctypes.c_size_t)
+GlobalAlloc.restype = wintypes.HGLOBAL
+GlobalSize = kernel32.GlobalSize
+GlobalSize.argtypes = wintypes.HGLOBAL,
+GlobalSize.restype = ctypes.c_size_t
+
+GMEM_MOVEABLE = 0x0002
+GMEM_ZEROINIT = 0x0040
+
+unicode = type(u'')
 
 
-class HelloWorld(FlowLauncher):
+def put(s):
+    if not isinstance(s, unicode):
+        s = s.decode('mbcs')
+    data = s.encode('utf-16le')
+    OpenClipboard(None)
+    EmptyClipboard()
+    handle = GlobalAlloc(GMEM_MOVEABLE | GMEM_ZEROINIT, len(data) + 2)
+    pcontents = GlobalLock(handle)
+    ctypes.memmove(pcontents, data, len(data))
+    GlobalUnlock(handle)
+    SetClipboardData(CF_UNICODETEXT, handle)
+    CloseClipboard()
+
+
+######
+
+class ObsidianPlugins(Flox):
 
     def query(self, query):
-        return [
-            {
-                "Title": "Hello World, this is where title goes. {}".format(('Your query is: ' + query , query)[query == '']),
-                "SubTitle": "This is where your subtitle goes, press enter to open Flow's url",
-                "IcoPath": "Images/app.png",
-                "JsonRPCAction": {
-                    "method": "open_url",
-                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher"]
-                }
-            }
-        ]
+        #using my own api here, just because(the raw json from gh would also work)
+        url = "https://api.obsidian.joethei.xyz/plugins"
+        data = urllib.request.urlopen(url).read().decode()
+        obj = json.loads(data)
+        var = filter(lambda x: self.args.lower() in x['name'].lower()
+                               or self.args.lower() in x['author'].lower()
+                               or self.args.lower() in x['description'].lower()
+                               or self.args.lower() in x['id'].lower()
+                     , obj)
+
+        for plugin in var:
+            self.add_item(title=plugin['name'], subtitle=plugin['description'], method=self.paste,
+                          parameters=[plugin['name'], plugin['id'], plugin['repo']])
 
     def context_menu(self, data):
-        return [
-            {
-                "Title": "Hello World Python's Context menu",
-                "SubTitle": "Press enter to open Flow the plugin's repo in GitHub",
-                "IcoPath": "Images/app.png",
-                "JsonRPCAction": {
-                    "method": "open_url",
-                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher.Plugin.HelloWorldPython"]
-                }
-            }
-        ]
+        self.add_item(
+            title=data,
+            subtitle=data
+        )
 
-    def open_url(self, url):
-        webbrowser.open(url)
+    def paste(self, name, pluginId, repo):
+        put("_" + name + "_(<obsidian://show-plugin?id=" + pluginId + ">|" + "<https://github.com/" + repo + ">)")
+
 
 if __name__ == "__main__":
-    HelloWorld()
+    ObsidianPlugins()
